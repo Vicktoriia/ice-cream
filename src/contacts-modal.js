455726fa63aa2c5b28656956785c7396b569adf3
Viktoriia Aleksieieva <105809872+Vicktoriia@users.noreply.github.com>
2023-03-28 11:37:34 +0300

    Initial commit

---
+++ src/contacts-modal.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-contacts-open]"),
+    closeModalBtn: document.querySelector("[data-modal-contacts-close]"),
+    modal: document.querySelector("[data-modal-contacts]"),
+    body: document.querySelector("[data-body-contacts]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
